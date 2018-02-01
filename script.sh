44320b36b046c022bceffbef6694c94853b6ac10
ashishbansal689 <ashish.kumar@decimal.co.in>
2018-02-01 15:00:45 +0530

    Commited newly added or changed files

--- script.sh
+++ script.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo 'Checking the current status of the project'
+echo -e 'Checking the current status of the project\n'
 STRING=`git status`
 echo $STRING
 if [[ "$STRING" == *"Changes"* ]]
 then
-	echo 'Some changes found'
-	echo 'Adding all the changed files to commit'
+	echo -e 'Some changes found\n'
+	echo -e 'Adding all the changed files to commit\n'
 	`git add .`
-	echo 'Commiting files to push to origin'
+	echo -e 'Commiting files to push to origin\n'
 	`git commit -m "Commited newly added or changed files"`
 	CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-	echo 'Current branch is: '$CURRENT_BRANCH
-	echo 'pushing changes to '$CURRENT_BRANCH
+	echo -e 'Current branch is: '$CURRENT_BRANCH \n
+	echo -e 'pushing changes to '$CURRENT_BRANCH \n
 	`git push origin $CURRENT_BRANCH`
 	echo 'File has been sycned with server successfully'
 else
