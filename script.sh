cfb3815f8679a8d58c381b65387c6fcbbdd49d6c
ashishbansal689 <ashish.kumar@decimal.co.in>
2018-02-01 15:01:33 +0530

    Commited newly added or changed files

--- script.sh
+++ script.sh
@@ -10,8 +10,8 @@ then
 	echo -e 'Commiting files to push to origin\n'
 	`git commit -m "Commited newly added or changed files"`
 	CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
-	echo -e 'Current branch is: '$CURRENT_BRANCH \n
-	echo -e 'pushing changes to '$CURRENT_BRANCH \n
+	echo -e 'Current branch is: '$CURRENT_BRANCH '\n'
+	echo -e 'pushing changes to '$CURRENT_BRANCH '\n'
 	`git push origin $CURRENT_BRANCH`
 	echo 'File has been sycned with server successfully'
 else
