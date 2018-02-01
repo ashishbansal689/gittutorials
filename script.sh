a569614013500c83dea0342c2e29a249898e31be
ashishbansal689 <ashish.kumar@decimal.co.in>
2018-02-01 14:20:43 +0530

    Commited newly added or changed files

--- script.sh
+++ script.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-echo 'Hello this is sample file'
-
-echo 'taking pull and merge in project'
-
-echo `git remote -v`
-
+echo 'Checking the current status of the project'
 STRING=`git status`
 echo $STRING
 if [[ "$STRING" == *"Changes"* ]]
@@ -14,9 +9,11 @@ then
 	`git add .`
 	echo 'Commiting files to push to origin'
 	`git commit -m "Commited newly added or changed files"`
-	echo 'pushing changes to server'
-	`git push origin dev`
+	CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+	echo 'Current branch is: '$CURRENT_BRANCH
+	echo 'pushing changes to '$CURRENT_BRANCH
+	`git push origin $CURRENT_BRANCH`
 	echo 'File has been sycned with server successfully'
 else
-	echo 'nothing to commit'
+	echo 'There is nothing to commit'
 fi
